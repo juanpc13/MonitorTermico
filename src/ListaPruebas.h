93eafbfd62d08b4d9ae3035f79e708d92b2b2c95
Juan Pleitez <juanpc13lolol@gmail.com>
2023-10-09 21:47:41 -0600

    timeToWatch

--- src/ListaPruebas.h
+++ src/ListaPruebas.h
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
 struct PruebaTermica {
-  String material;
+  String material;//7 Caracteres Max
   int temperaturaTarget;
-  int tiempoMiliseconds;
+  int tiempo;
 };
 
 #define TOTAL_PRUEBAS 3
@@ -11,15 +11,15 @@ PruebaTermica pruebasTermicas[TOTAL_PRUEBAS];
 
 void CargarListaPruebas()
 {
-    pruebasTermicas[0].material = "Aluminio";
+    pruebasTermicas[0].material = "1045";
     pruebasTermicas[0].temperaturaTarget = 800;
-    pruebasTermicas[0].material = 60000;
+    pruebasTermicas[0].tiempo = 60;
 
-    pruebasTermicas[1].material = "Plastico";
+    pruebasTermicas[1].material = "1046";
     pruebasTermicas[1].temperaturaTarget = 300;
-    pruebasTermicas[1].material = 30000;
+    pruebasTermicas[1].tiempo = 30;
 
-    pruebasTermicas[2].material = "Hierro";
+    pruebasTermicas[2].material = "1047";
     pruebasTermicas[2].temperaturaTarget = 1200;
-    pruebasTermicas[2].material = 120000;
+    pruebasTermicas[2].tiempo = 120;
 }
--- src/Menus.h
+++ src/Menus.h
@@ -6,23 +6,83 @@ void PantallaInicial()
 {
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("Hello, world!");
+    lcd.print("UES * FIA * EIM ");
     lcd.setCursor(0,1);
-    lcd.print("Hello, world!");
+    lcd.print("  Velocidad de  ");
     lcd.setCursor(0,2);
-    lcd.print("Hello, world!");
+    lcd.print(" Calentamiento  ");
     lcd.setCursor(0,3);
-    lcd.print("Hello, world!");
+    lcd.print("Inicio - Apagar ");
+}
+
+bool NeedZeroDigit(int val)
+{
+    return val < 10;
+}
+
+String timeToWatch(int segundos)
+{
+    int horas = segundos / 3600;          // 1 hora = 3600 segundos
+    int minutos = (segundos % 3600) / 60; // 1 minuto = 60 segundos
+    int segundosRestantes = segundos % 60;
+
+    String horaTxt = "";
+    if(NeedZeroDigit(horas)){horaTxt += "0";}
+    horaTxt += horas;
+    horaTxt += ":";
+    if(NeedZeroDigit(minutos)){horaTxt += "0";}
+    horaTxt += minutos;
+    if(NeedZeroDigit(segundosRestantes)){horaTxt += "0";}
+    horaTxt += ":";
+    horaTxt += segundosRestantes;
+    return horaTxt;
 }
 
 void PantallaSelecionarPrueba()
 {
-    
+    PruebaTermica prueba = pruebasTermicas[TOTAL_PRUEBAS];
+    lcd.clear();
+    lcd.setCursor(0,0);
+    String salida = "";
+    salida = "Material:";
+    salida += prueba.material;
+    lcd.print(salida);
+    lcd.setCursor(0,1);
+    salida = "Temp:";
+    salida += prueba.temperaturaTarget;
+    salida += "C";
+    lcd.print(salida);
+    lcd.setCursor(0,2);
+    salida = "Tiempo:";
+    salida += timeToWatch(prueba.tiempo);
+    lcd.print(salida);
+    lcd.setCursor(0,3);
+    lcd.print("Iniciar-Cambiar ");
 }
 
 void PantallaEstadoPrueba()
 {
-    
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("TermoCupla:9999C");
+    lcd.setCursor(0,1);
+    lcd.print("Total - 99:59:59");
+    lcd.setCursor(0,2);
+    lcd.print("Probeta-99:59:59");
+    lcd.setCursor(0,3);
+    //PAUSAR-CONTINUAR-INICIO
+    if(estadoPrueba == 0)
+    {
+        lcd.print("Pausa - Detener ");
+    }
+    else if(estadoPrueba == 1)
+    {
+        lcd.print("Continua-Detener");
+    }
+    else if(estadoPrueba == 2)
+    {
+        lcd.print("Inicio - Apagar ");
+    }
 }
 
 int pantallaActual = 0;
