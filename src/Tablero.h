456ee4f510dd98e0c3c28b5ef4a0d22ea9ce4245
Juan Pleitez <juanpc13lolol@gmail.com>
2023-10-20 21:45:20 -0600

    cambio de pines

--- src/Tablero.h
+++ src/Tablero.h
@@ -10,7 +10,7 @@ char keys[ROWS][COLS] = {
   {'#','0','*','D'}
 };
 
-byte rowPins[ROWS] = {5, 4, 3, 2};
-byte colPins[COLS] = {9, 8, 7, 6};
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
